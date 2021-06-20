683ea17da33e8ea7b39c8dbb68fa8689fd0fa05d
mayyadah <miss.mayadah1@gmail.com>
2021-06-20 19:37:18 +0300

    write lab1

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <footer>
+                    &copy; Mayyadah
+                </footer>
+            </div>
+        )
+    }
+}
+
+export default Footer
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+    render() {
+        return (
+            <div><header>
+                <h1>Welcome in Horned Beast </h1>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default Header
