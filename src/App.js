760167ff6dd7ba61b8d0d1d9a710514eebb1d1c9
mayyadah <miss.mayadah1@gmail.com>
2021-06-21 20:59:24 +0300

    lab2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
-import Header from "./Header";
-import Footer from "./Footer";
-import Main from "./Main";
+import React, { Component } from 'react';
+import Header from './src/Header.js';
+import Footer from './src/Footer.js';
+import Main from './src//Main.js';
 
-export class App extends Component {
+ class App extends Component {
   render() {
     return (
       <div>
